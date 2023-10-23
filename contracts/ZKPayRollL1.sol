ee40ae24b2f3a89fac1e55bcf9808408108e4c21
lei-1993 <lei@movemnt.dev>
2023-10-24 00:44:34 +0800

    add linea

--- contracts/ZKPayRollL1.sol
+++ contracts/ZKPayRollL1.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 interface IZKSyncL1bridge {
-    function depositERC20(
+    function deposit(
         address _l2Receiver,
         address _l1Token,
         uint256 _amount,
@@ -35,19 +37,28 @@ interface IScrollGateway {
     ) external payable;
 }
 
-contract ZKPayRollL1 {
+interface ILineaGateway {
+    function depositTo(uint256 amount,address to) external;
+}
+
+contract ZKPayRollL1 is Ownable {
     event TransferCommited(address sender, uint totalAmount, address token, uint index, uint chainId);
 
     using SafeERC20 for IERC20;
+    using SafeMath for uint;
 
     uint constant public ZKSYNC = 1;
     uint constant public SCROLL = 2;
+    uint constant public LINEA = 3;
 
     uint constant public gasPerPubdataByte = 800;
     address public zksync_bridge = 0x927DdFcc55164a59E0F33918D13a2D559bC10ce7;
     address public zkSync_api = 0x1908e2BF4a88F91E4eF0DC72f02b8Ea36BEa2319;
 
     address public scroll_gateway = 0x65D123d6389b900d954677c26327bfc1C3e88A13;
+
+    mapping (uint => uint) public txFees;
+    mapping (address => uint) public tokenFees;
 
     receive() external payable {
 
@@ -57,10 +68,30 @@ contract ZKPayRollL1 {
         fee = IZKSync(zkSync_api).l2TransactionBaseCost(gasPrice, gasUsage, gasPerPubdataByte);
     }
 
+    function setFees(uint chainId, uint fee) external onlyOwner {
+        txFees[chainId] = fee;
+    }
+
+    function withdrawFee(address token) external {
+        if(token == address(0)) {
+            payable(owner()).transfer(address(this).balance);
+        } else {
+            uint totalFee = tokenFees[token];
+            IERC20(token).safeTransfer(owner(), totalFee);
+            tokenFees[token] = 0;
+        }
+        
+    }
+
     function commitTransfer(address l2Contract, address token, uint nonce, uint chainId, uint amount, uint gasUsage) external payable {
         (, bytes memory data) = token.staticcall(
                 abi.encodeWithSignature("decimals()")
             );
+        uint fee = txFees[chainId];
+        if(fee > 0) {
+            amount = amount.sub(fee);
+            tokenFees[token] = tokenFees[token].add(fee);
+        }
         uint8 decimals = abi.decode(data, (uint8));
         require(decimals >= 6, "only support decimal greater than 4");
         require(nonce < 10000, "Invalid nonce");
@@ -70,13 +101,19 @@ contract ZKPayRollL1 {
             IERC20(token).safeTransferFrom(msg.sender, address(this), actualAmount);
             if(chainId == ZKSYNC) {
                 IERC20(token).approve(address(zksync_bridge), actualAmount);
-                IZKSyncL1bridge(zksync_bridge).depositERC20{value: msg.value}(l2Contract, token, actualAmount, gasUsage, gasPerPubdataByte, msg.sender);
-                emit TransferCommited(msg.sender, amount, token, nonce, chainId);
+                IZKSyncL1bridge(zksync_bridge).deposit{value: msg.value}(l2Contract, token, actualAmount, gasUsage, gasPerPubdataByte, msg.sender);
             } else if(chainId == SCROLL) {
                 IERC20(token).approve(address(scroll_gateway), actualAmount);
                 IScrollGateway(scroll_gateway).depositERC20{value: msg.value}(token, l2Contract, actualAmount, gasUsage);
-                emit TransferCommited(msg.sender, amount, token, nonce, chainId);
+            } else if(chainId == LINEA) {
+                // require(bridges[chainId][token] != address(0), "not support now");
+                // address linea_bridge = bridges[chainId][token];
+                // IERC20(token).approve(address(linea_bridge), actualAmount); 
+                // ILineaGateway(linea_bridge).depositTo(actualAmount, l2Contract);
+            } else {
+                require(false, "Invalid chain id");
             }
+            emit TransferCommited(msg.sender, amount, token, nonce, chainId);
         }
     }
 }
